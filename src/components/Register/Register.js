f48414c6e83f53545a882c7273b04dbfee7c6a83
Isai Lopez <ilopezro@ucsc.edu>
2019-03-13 00:05:10 -0700

    styled register page; need to work on footer staying at bottom

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import './register.css'
 
 class Register extends Component {
 
@@ -10,24 +11,24 @@ class Register extends Component {
 
     render() {
       return (
-        <form action = "/signUpUser" method = "POST">
+        <form className = "registerUserForm" action = "/signUpUser" method = "POST">
         <h1>Welcome to Registration Page. </h1>
         <h4>Please fill out form and submit. Give an answer to every starred box. </h4> 
-          <input placeholder="First Name" name = "firstName" required/>
-          <input placeholder="Last Name" name = "lastName" required/>
+          <input className = "firstNameInput" placeholder="First Name" name = "firstName" required/>
+          <input className = "lastNameInput" placeholder="Last Name" name = "lastName" required/>
           <br/>
-          <input placeholder="Phone Number" name = "phoneNumber"/>
-          <input placeholder="Email" name = "email" required/>
+          <input className = "phoneNumberInput" placeholder="Phone Number" name = "phoneNumber"/>
+          <input className = "emailInput"placeholder="Email" name = "email" required/>
           <br/>
-          <input placeholder="Major" name = "studentMajor" required />
-          <input placeholder="Student ID" name = "studentID" required/>
+          <input className = "majorInput" placeholder="Major" name = "studentMajor" required />
+          <input className = "idInput" placeholder="Student ID" name = "studentID" required/>
           <br/>
-          <input placeholder = "age" name = "studentAge" required/>
+          <input className = "ageInput" placeholder = "age" name = "studentAge" required/>
           <br/>
           <br/>
           <label>
             Do you have any dietary restrictions? *
-            <select name = "dietSelect"> 
+            <select className = "dietInput" name = "dietSelect"> 
               <option value="null"> </option>
               <option name = "dietYes" value="yes">Yes</option>
               <option name = "dietNo" value="no">No</option>
@@ -36,12 +37,12 @@ class Register extends Component {
           <br/>
         If yes, please tell us below. 
         <br/>
-        <textarea name = "dietDescription"></textarea>
+        <textarea className = "dietTxt" name = "dietDescription"></textarea>
         <br/>
         <br/>
           <label>
           Do you have any physical disabilities that we should know of? *
-            <select name = "physSelect">
+            <select className = "physInput" name = "physSelect">
               <option value="null"> </option>
               <option name = "physYes" value="yes">Yes</option>
               <option name = "physNo" value="no">No</option>
@@ -50,9 +51,9 @@ class Register extends Component {
         <br/>
         If yes, please tell us below. 
         <br/>
-        <textarea name = "physDescription"></textarea>
+        <textarea className = "physTxt" name = "physDescription"></textarea>
           <br/>
-          <button>Create</button>
+          <button className = "button">Create</button>
         </form>
       );
     }
