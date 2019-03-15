215fdd42f7b10fa0482d6823e1a8721f3bbe5821
Isai Lopez <ilopezro@ucsc.edu>
2019-03-14 18:03:25 -0700

    changed select input on register page to make it smaller and added * to all required components

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,6 +6,8 @@ class Register extends Component {
   constructor(props){
     super(props)
     this.state = {
+      physSelect: "",
+      physRequired: false
     }
   }
 
@@ -14,21 +16,21 @@ class Register extends Component {
         <form className = "registerUserForm" action = "/signUpUser" method = "POST">
         <h1>Welcome to Registration Page. </h1>
         <h4>Please fill out form and submit. Give an answer to every starred box. </h4> 
-          <input className = "firstNameInput" placeholder="First Name" name = "firstName" required/>
-          <input className = "lastNameInput" placeholder="Last Name" name = "lastName" required/>
+          <input className = "firstNameInput" placeholder="First Name *" name = "firstName" required/>
+          <input className = "lastNameInput" placeholder="Last Name *" name = "lastName" required/>
+          <br/>
+          <input className = "ageInput" placeholder = "Age *" name = "studentAge" required/>
+          <input className = "emailInput"placeholder="Email *" name = "email" required/>
+          <br/>
+          <input className = "majorInput" placeholder="Major *" name = "studentMajor" required/>
+          <input className = "idInput" placeholder="Student ID *" name = "studentID" required/>
           <br/>
           <input className = "phoneNumberInput" placeholder="Phone Number" name = "phoneNumber"/>
-          <input className = "emailInput"placeholder="Email" name = "email" required/>
-          <br/>
-          <input className = "majorInput" placeholder="Major" name = "studentMajor" required />
-          <input className = "idInput" placeholder="Student ID" name = "studentID" required/>
-          <br/>
-          <input className = "ageInput" placeholder = "age" name = "studentAge" required/>
           <br/>
           <br/>
           <label>
             Do you have any dietary restrictions? *
-            <select className = "dietInput" name = "dietSelect"> 
+            <select className = "dietInput" name = "dietSelect" required> 
               <option value="null"> </option>
               <option name = "dietYes" value="yes">Yes</option>
               <option name = "dietNo" value="no">No</option>
