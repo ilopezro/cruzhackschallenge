9e4ae93c6cd5742453c34a7622307cdf9fdc85b3
Isai Lopez <ilopezro@ucsc.edu>
2019-03-15 01:17:19 -0700

    testing different folder

--- database/index.js
+++ database/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 app.use(cors())
 
-app.use(express.static('./public'))
+app.use(express.static('../public'))
 app.use(morgan('short'))
 
 app.use(bodyParser.urlencoded({extended: false}))
